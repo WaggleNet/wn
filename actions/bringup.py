5e0a0a4d9db4668230cc7c6b63123138c1a19bc7
Streamliner <jimmy.xrail@me.com>
2019-11-05 17:35:54 -0600

    Dumping iam_appids into env.yaml

--- actions/bringup.py
+++ actions/bringup.py
@@ -1,4 +1,5 @@
 import requests
+import yaml
 
 from services.actions import Action
 from services.util import generate_keypairs
@@ -19,11 +20,17 @@ def create_app(name, url=''):
     # Now update the envfile
     with open('{}/data/envs/{}.env'.format(src, name), 'w') as f:
         f.write('IAM_APP_ID=%s' % app_id)
+    return app_id
 
 
 @Action
 def create_iam_app_keys():
-    create_app('backplane')
-    create_app('erp', 'http://localhost:15010/callback')
-    create_app('wharf')
-    create_app('frontier', 'http://localhost:15020/callback')
+    appids = {
+        'backplane': create_app('backplane'),
+        'erp': create_app('erp', 'http://localhost:15010/callback'),
+        'wharf': create_app('wharf'),
+        'frontier': create_app('frontier', 'http://localhost:15020/callback')
+    }
+    src = get_source_dir()
+    with open('{}/data/envs/env.yaml'.format(src), 'w') as f:
+        yaml.dump(appids, f)
